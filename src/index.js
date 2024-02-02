207cd5c5e7a5f9aff1c55cb95be8dffcc37ee3cb
kix-mix <kixjmedia@gmail.com>
2024-02-01 22:49:12 -0500

    Added basic search function with API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import WeatherSearch from "./WeatherSearch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <div className="App">
+      <App />
+      <WeatherSearch />
+    </div>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
